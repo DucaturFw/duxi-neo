41b8057312a033e9bf59773ef97c1d6e57374d1d
zlumer <zlumer@gmail.com>
2018-09-18 21:54:00 +0300

    extended ping (getapplicationlog)

--- ping.sh
+++ ping.sh
@@ -1,2 +1,2 @@
 curl -XPOST -H "Content-type: application/json" -d '{"jsonrpc": "2.0", "method": "getblockcount", "params": [], "id": 1}' 'http://localhost:10332'
-
+# curl -XPOST -H "Content-type: application/json" -d '{"jsonrpc": "2.0", "method": "getapplicationlog", "params": [], "id": 1}' 'http://localhost:10332'
